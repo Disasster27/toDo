824a7715eee7fb1b2c98eefcd07b2e7efc3e2924
Disasster27 <sf_dis@mail.ru>
2020-02-25 22:14:25 +0300

    add todo

--- src/index.js
+++ src/index.js
@@ -10,11 +10,35 @@ let todoIdCounter = 0;
 const addButton = document.querySelectorAll( '.menu__button' );
 const todoContainer = document.querySelector( '.todo-container' );
 
-document.querySelectorAll( '.todo' ).forEach( todoElement => {
+document.querySelectorAll( '.todo' ).forEach( setEvent );
+
+document.querySelector( '.menu__button' ).addEventListener( 'click', event => {
+	console.log( this );
+	
+	const newTodo = document.createElement( 'div' );
+	newTodo.classList.add( 'todo' );
+	newTodo.setAttribute( 'draggable', 'true' );
+	newTodo.setAttribute( 'data-todo-id', todoIdCounter );
+	newTodo.innerHTML = `<div class="todo__header">
+					<p>To do</p>
+				</div>
+				<div class="todo__footer">
+					<div class="todo__add-task">
+						+ Add new task
+					</div>
+					<div class="todo__delete-button">
+						<button>X</button>
+					</div>
+				</div>`;
+	todoIdCounter++;
+	document.querySelector( '.todo-container' ).insertAdjacentElement( 'beforeend', newTodo );
+	setEvent ( newTodo );
+} );
+
+function setEvent ( todoElement ) {
+	console.log( todoElement );
 	const addTaskButton = todoElement.querySelector( '.todo__add-task' );
 	addTaskButton.addEventListener( 'click', function ( event ) {
-		
-		console.log( this );
 		
 		const newTask = `<div class="todo__task" draggable="true" data-task-id="${taskIdCounter}">
 					<div class="todo__data">
@@ -37,28 +61,6 @@ document.querySelectorAll( '.todo' ).forEach( todoElement => {
 		taskIdCounter++;
 		
 		todoElement.querySelector( '.todo__footer' ).insertAdjacentHTML( 'beforebegin', newTask );
-		
 	} );
-} );
-
-document.querySelector( '.menu__button' ).addEventListener( 'click', event => {
-	console.log( this );
-	const newTodo = `<div class="todo" data-todo-id="${todoIdCounter}" draggable="true">
-				<div class="todo__header">
-					<p>To do</p>
-				</div>
-				<div class="todo__footer">
-					<div class="todo__add-task">
-						+ Add new task
-					</div>
-					<div class="todo__delete-button">
-						<button>X</button>
-					</div>
-				</div>
-			</div>`;
-	todoIdCounter++;
-	document.querySelector( '.todo-container' ).insertAdjacentHTML( 'beforeend', newTodo );
-	
-} )
-
+}
 
