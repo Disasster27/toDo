883102b6853d07bb9a3b9485cc77549ffd306144
Disasster27 <sf_dis@mail.ru>
2020-02-26 00:03:53 +0300

    add contenteditable

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ let todoIdCounter = 0;
 const addButton = document.querySelectorAll( '.menu__button' );
 const todoContainer = document.querySelector( '.todo-container' );
 
-document.querySelectorAll( '.todo' ).forEach( setEvent );
+document.querySelectorAll( '.todo' ).forEach( setAddNewTask );
 
 document.querySelector( '.menu__button' ).addEventListener( 'click', event => {
-	console.log( this );
+//	console.log( this );
 	
 	const newTodo = document.createElement( 'div' );
 	newTodo.classList.add( 'todo' );
 	newTodo.setAttribute( 'draggable', 'true' );
 	newTodo.setAttribute( 'data-todo-id', todoIdCounter );
 	newTodo.innerHTML = `<div class="todo__header">
-					<p>To do</p>
+					<p class="todo__note">To do</p>
 				</div>
 				<div class="todo__footer">
 					<div class="todo__add-task">
@@ -32,35 +32,54 @@ document.querySelector( '.menu__button' ).addEventListener( 'click', event => {
 				</div>`;
 	todoIdCounter++;
 	document.querySelector( '.todo-container' ).insertAdjacentElement( 'beforeend', newTodo );
-	setEvent ( newTodo );
+//	console.log( newTodo );
+	setAddNewTask ( newTodo );
+	newTodo.querySelectorAll( '.todo__note' ).forEach( setContenteditable );
 } );
 
-function setEvent ( todoElement ) {
-	console.log( todoElement );
+document.querySelectorAll( '.todo__note' ).forEach( setContenteditable );
+
+function setAddNewTask ( todoElement ) {
+//	console.log( todoElement );
 	const addTaskButton = todoElement.querySelector( '.todo__add-task' );
 	addTaskButton.addEventListener( 'click', function ( event ) {
 		
-		const newTask = `<div class="todo__task" draggable="true" data-task-id="${taskIdCounter}">
-					<div class="todo__data">
-						<p>12.02</p>
-						<div class="todo__delete-button">
-							<button>X</button>
-						</div>
+		const newTask = document.createElement( 'div' );
+		newTask.classList.add( 'todo__task' );
+		newTask.setAttribute( 'draggable', 'true' );
+		newTask.setAttribute( 'data-todo-id', todoIdCounter );
+		newTask.innerHTML = `<div class="todo__data">
+					<p>12.02</p>
+					<div class="todo__delete-button">
+						<button>X</button>
 					</div>
-					<div class="todo__task-text">
-						<p>Доделать до конца</p>
-					</div>
-					<div class="todo__tags">
-						<span class="tags">task</span>
-						<span class="tags">interaction</span>
-						<span class="tags">related</span>
-						<span class="tags">Due date</span>
-					</div>
+				</div>
+				<div class="todo__task-text">
+					<p class="todo__note"></p>
+				</div>
+				<div class="todo__tags">
+					<span class="tags">task</span>
+					<span class="tags">interaction</span>
+					<span class="tags">related</span>
+					<span class="tags">Due date</span>
 				</div>`;
 		
 		taskIdCounter++;
 		
-		todoElement.querySelector( '.todo__footer' ).insertAdjacentHTML( 'beforebegin', newTask );
+		todoElement.querySelector( '.todo__footer' ).insertAdjacentElement ( 'beforebegin', newTask );
+		console.log( newTask );
+		newTask.querySelectorAll( '.todo__note' ).forEach( setContenteditable );
 	} );
-}
+};
 
+function setContenteditable ( noteElement ) {
+//	console.log(noteElement);
+	noteElement.addEventListener( 'dblclick', ( event ) => {
+		console.log(noteElement)
+		noteElement.setAttribute( 'contenteditable', 'true' );
+		noteElement.focus();
+	} );
+	noteElement.addEventListener( 'blur', ( event ) => {
+		noteElement.removeAttribute( 'contenteditable' );
+	} );
+};
