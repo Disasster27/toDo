fa14b896d49e40346a5e2a7ff743845c2005255b
Disasster27 <sf_dis@mail.ru>
2020-02-25 20:10:50 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -3,14 +3,70 @@ import './scss/main.scss';
 //import './css/reset.css';
 //import src from './images/central_park.jpg'
 
+let taskIdCounter = 0;
+let todoIdCounter = 0;
 
 
-console.log( ( ( a ) => a )(2) );
+const addButton = document.querySelectorAll( '.menu__button' );
+const todoContainer = document.querySelector( '.todo-container' );
 
-let foo = function ( q ) {
-	return (  ) => q * q;
-};
+document.querySelectorAll( '.todo' ).forEach( todoElement => {
+	const addTaskButton = todoElement.querySelector( '.todo__add-task' );
+	addTaskButton.addEventListener( 'click', function ( event ) {
+		console.log( this );
+		
+		const newTask = `<div class="todo__task" draggable="true" data-task-id="${taskIdCounter}">
+					<div class="todo__data">
+						<p>12.02</p>
+						<div class="todo__delete-button">
+							<button>X</button>
+						</div>
+					</div>
+					<div class="todo__task-text">
+						<p>Доделать до конца</p>
+					</div>
+					<div class="todo__tags">
+						<span class="tags">task</span>
+						<span class="tags">interaction</span>
+						<span class="tags">related</span>
+						<span class="tags">Due date</span>
+					</div>
+				</div>`;
+		taskIdCounter++;
+		
+		todoElement.querySelector( '.todo__footer' ).insertAdjacentHTML( 'beforebegin', newTask );
+		
+	} )
+} )
 
-let foo2 = foo(2)();
 
-console.log ( foo2 );
+
+
+
+
+
+
+////console.log( todoContainer );
+//
+//for ( let i = 0 ; i < addButton.length ; i++ ) {
+//	addButton[i].addEventListener( 'click', () => { addTodo() } );
+//}
+//
+//
+//function addTodo () {
+//	
+//	
+//	
+////	console.log( 2 );
+//	
+//}
+//
+//class todo {
+//	constructor () {
+//		
+//	}
+//	
+//	render () {
+//		
+//	}
+//}
